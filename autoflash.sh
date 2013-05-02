39168bd29bd515b8200d3d2c3b186867720a6583
Al Tsai[:atsai] <atsai@mozilla.com>
2013-05-02 15:07:18 +0800

    add more version support

--- autoflash.sh
+++ autoflash.sh
@@ -34,6 +34,7 @@
 #   2013/03/11 Al:      v10.3 Add auto prompt
 #   2013/04/09 Al:      v11.0 Add new devices leo, inari
 #   2013/04/10 Al:      v11.1 Add new devices hamachi (a.k.a. buri)
+#   2013/05/02 Al:      v11.2 Add other version support
 #
 # = = = = = = = = = = = Backlog = = = = = = = = = = = = = = = = = = = = = =
 #   2013/04/09 Al:      Need to refactor "Check File" section
@@ -258,7 +259,9 @@ elif [ $Device_Flag == "leo" ]; then
 	DownloadFilename=leo.zip
 	Engineer_Flag=0
 	# there is v1-train for leo device only
-	if [ $Version_Flag == "v1train" ]; then
+	if [ $Version_Flag == "v1train" ] && [ $Engineer_Flag == 1 ]; then
+		URL=https://pvtbuilds.mozilla.org/pvt/mozilla.org/b2gotoro/nightly/mozilla-b2g18-leo-eng/latest/${DownloadFilename}
+	elif [ $Version_Flag == "v1train" ] && [ $Engineer_Flag == 0 ]; then
 		URL=https://pvtbuilds.mozilla.org/pvt/mozilla.org/b2gotoro/nightly/mozilla-b2g18-leo/latest/${DownloadFilename}
 	else
 		echo -e "There is v1-train (v1.1.0) for leo device only"
@@ -268,7 +271,9 @@ elif [ $Device_Flag == "inari" ]; then
 	DownloadFilename=inari.zip
 	Engineer_Flag=0
 	# there are shira and v1-train available for inari device
-	if [ $Version_Flag == "shira" ]; then
+	if [ $Version_Flag == "shira" ] && [ $Engineer_Flag == 1 ]; then
+		URL=https://pvtbuilds.mozilla.org/pvt/mozilla.org/b2gotoro/nightly/mozilla-b2g18_v1_0_1-inari-eng/latest/${DownloadFilename}
+	elif [ $Version_Flag == "shira" ] && [ $Engineer_Flag == 0 ]; then
 		URL=https://pvtbuilds.mozilla.org/pvt/mozilla.org/b2gotoro/nightly/mozilla-b2g18_v1_0_1-inari/latest/${DownloadFilename}
 	elif [ $Version_Flag == "v1train" ]; then
 		URL=https://pvtbuilds.mozilla.org/pvt/mozilla.org/b2gotoro/nightly/mozilla-b2g18-inari/latest/${DownloadFilename}
@@ -293,8 +298,12 @@ elif [ $Device_Flag == "buri" ] || [ $Device_Flag == "hamachi" ]; then
 	# v1-train: eng/user build
 	if [ $Version_Flag == "v1train" ]; then
 		URL=https://pvtbuilds.mozilla.org/pvt/mozilla.org/b2gotoro/nightly/mozilla-b2g18-hamachi/latest/${DownloadFilename}
+	elif [ $Version_Flag == "shira" ] && [ $Engineer_Flag == 1 ] ; then
+		URL=https://pvtbuilds.mozilla.org/pvt/mozilla.org/b2gotoro/nightly/mozilla-b2g18_v1_0_1-hamachi-eng/latest/${DownloadFilename}
+	elif [ $Version_Flag == "shira" ] && [ $Engineer_Flag == 0 ] ; then
+		URL=https://pvtbuilds.mozilla.org/pvt/mozilla.org/b2gotoro/nightly/mozilla-b2g18_v1_0_1-hamachi/latest/${DownloadFilename}		
 	else
-		echo -e "There is v1-train (v1.1.0) for buri device"
+		echo -e "There is v1-train (v1.1.0) and shira (v1.0.1) available  for buri device"
 	fi
 fi
 
