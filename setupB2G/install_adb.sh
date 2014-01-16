c2b773286c2defb2be987f1e731cb6dd83fdc4f7
askeing <askeing@gmail.com>
2014-01-16 15:56:43 +0800

    Updated script of install adb

--- setupB2G/install_adb.sh
+++ setupB2G/install_adb.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-# Create temp folder
-TEMP_DIR=$(mktemp -d)
-
-# Clone adb fastboot install project
-cd $TEMP_DIR
-git clone https://github.com/teamblueridge/adb-fastboot-install.git
-
-# Install adb into system
-cd adb-fastboot-install
-./ADB-Install-Linux.sh
-
-# Clean temp folder
-cd ~
-rm -rf $TEMP_DIR
+sudo add-apt-repository ppa:nilarimogard/webupd8
+sudo apt-get update
+sudo apt-get install android-tools-adb android-tools-fastboot
+echo -e "### Please edit \"/etc/udev/rules.d/51-android.rules\" to fit your requirements!"
