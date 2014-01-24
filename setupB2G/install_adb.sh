d53772a4709a0c01a3d065c37456c7437d947854
Askeing Yen (fyen) <askeing@gmail.com>
2014-01-24 14:27:20 +0800

    Update install_adb.sh

--- setupB2G/install_adb.sh
+++ setupB2G/install_adb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#http://www.webupd8.org/p/ubuntu-ppas-by-webupd8.html
 sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get update
 sudo apt-get install android-tools-adb android-tools-fastboot
