78b68f9c920ef444254a0cc46944779692bd8cdd
askeing <askeing@gmail.com>
2014-11-13 16:32:18 +0800

    Add python version backup_restore_profile.py

--- utilities/adb_helper.py
+++ utilities/adb_helper.py
@@ -3,11 +3,13 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import re
+import logging
 import subprocess
 from distutils import spawn
 
 
 class AdbHelper(object):
+    logger = logging.getLogger(__name__)
 
     @staticmethod
     def has_adb():
@@ -17,8 +19,11 @@ class AdbHelper(object):
 
     @staticmethod
     def adb_devices():
-        p = subprocess.Popen('adb devices', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        cmd = 'adb devices'
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = p.communicate()[0]
+        AdbHelper.logger.debug('cmd: {0}'.format(cmd))
+        AdbHelper.logger.debug('output: {0}'.format(output))
         output = re.sub(r'\r+', '', output)
         output_list = re.split(r'\n+', output)
         output_list = [item for item in output_list if item]
@@ -39,6 +44,24 @@ class AdbHelper(object):
         cmd = '%s %s %s' % (cmd, source, dest)
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = p.communicate()[0]
+        AdbHelper.logger.debug('cmd: {0}'.format(cmd))
+        AdbHelper.logger.debug('output: {0}'.format(output))
+        if p.returncode is not 0:
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def adb_push(source, dest, serial=None):
+        if serial is None:
+            cmd = 'adb push'
+        else:
+            cmd = 'adb -s %s push' % (serial,)
+        cmd = '%s %s %s' % (cmd, source, dest)
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = p.communicate()[0]
+        AdbHelper.logger.debug('cmd: {0}'.format(cmd))
+        AdbHelper.logger.debug('output: {0}'.format(output))
         if p.returncode is not 0:
             return False
         else:
@@ -53,4 +76,6 @@ class AdbHelper(object):
         cmd = '%s %s' % (cmd, command)
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = p.communicate()[0]
+        AdbHelper.logger.debug('cmd: {0}'.format(cmd))
+        AdbHelper.logger.debug('output: {0}'.format(output))
         return output
